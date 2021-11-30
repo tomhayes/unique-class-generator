ad6635e352665d4316928fe5a552918f0bc99ed0
Tom <hello@tomhay.es>
2021-11-30 14:05:19 +0000

    deleagated passing keystrokes to Automator, set string to 4 char lowercase

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import string
 import random
-import pyautogui
+# import pyautogui # we don't need this as it's handled by Automator
 import os.path
 from csv import writer
 
@@ -12,8 +12,11 @@ if not (os.path.exists(db)):
     open(db, 'a').close()
 
 # Classname generator function
-def class_generator(size=6, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+
+
+def class_generator(size=4, chars=string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
+
 
 # Generate classname
 classname = class_generator()
@@ -23,15 +26,16 @@ while classname in open(db).read():
     classname = class_generator()
 
 # Add classname to database CSV
-with open(db, 'a', newline='') as f_object:  
+with open(db, 'a', newline='') as f_object:
     # Put classname into a list (otherwise each letter becomes a value)
     classnamelist = [classname]
     # Pass the CSV  file object to the writer() function
     writer_object = writer(f_object)
     # Result - a writer object
     # Pass the classname as an argument into the writerow() function
-    writer_object.writerow(classnamelist)  
+    writer_object.writerow(classnamelist)
     # Close the file object
     f_object.close()
 
-pyautogui.write(classname)
+# pyautogui.write(classname) # this is handled by Automator now...
+print(classname)
